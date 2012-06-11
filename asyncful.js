84fd9efd073472bb1c5489bdcd5e96bb2151278c
Arthur Corenzan <arthur@corenzan.com>
2012-06-11 03:30:54 -0300

    fixed native submit not triggering the event

--- asyncful.js
+++ asyncful.js
@@ -1,48 +1,57 @@
 /*
- * Asyncful v0.6 2012-06-11 01:36:10 -0300
+ * Asyncful v0.6 2012-06-11 03:30:14 -0300
  * by Arthur Corenzan <arthur@corenzan.com>
  * licensed under http://creativecommons.org/licenses/by-sa/3.0
  * more on http://github.com/haggen/asyncful
  */
 (function($) {
   $.fn.asyncful = function(options) {
-    return this.on('submit', function(e) {
-      var self, enctype, frame, frameName;
+    return this.each(function() {
+      var self;
 
       self = $(this);
-      enctype = self.attr('enctype') || '';
 
-      if(enctype.toLowerCase() === 'multipart/form-data') {
-        frame = $('<iframe></iframe>');
+      // Form's native submit method does not trigger the event,
+      // so let's replace it with another that does
+      this.submit = self.submit;
 
-        frameName = 'frame-' + Math.random().toString(36).slice(2);
+      self.on('submit', function(e) {
+        var enctype, frame, frameName;
 
-        frame
-          .attr('name', frameName)
-          .attr('style', 'width:0;height:0;margin:0;border:0;padding:0;float:left')
-          .on('load', function() {
-            try {
-              eval($(this).contents().find('body').text());
-            } catch(e) {}
+        enctype = self.attr('enctype') || '';
+
+        if(enctype.toLowerCase() === 'multipart/form-data') {
+          frame = $('<iframe></iframe>');
+
+          frameName = 'frame-' + Math.random().toString(36).slice(2);
+
+          frame
+            .attr('name', frameName)
+            .attr('style', 'width:0;height:0;margin:0;border:0;padding:0;float:left')
+            .on('load', function() {
+              try {
+                eval($(this).contents().find('body').text());
+              } catch(e) {}
+            });
+
+          self.before(frame);
+
+          self.attr('target', frameName)
+        } else {
+          options = $.extend(options, {
+            url: this.action, 
+            type: this.method, 
+            dataType: 'script',
+            data: $(this).serialize(),
+            context: this
           });
 
-        self.before(frame);
+          e.preventDefault();
+          $.ajax(options);
 
-        self.attr('target', frameName)
-      } else {
-        options = $.extend(options, {
-          url: this.action, 
-          type: this.method, 
-          dataType: 'script',
-          data: $(this).serialize(),
-          context: this
-        });
-
-        e.preventDefault();
-        $.ajax(options);
-
-        return false;
-      }
+          return false;
+        }
+      });
     });
   };
 })(jQuery);
